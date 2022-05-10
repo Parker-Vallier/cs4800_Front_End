38815ae1978e7057ec7c6ec56fd30a8fe7650fd8
Rahul-Sharma-1234 <75908287+Rahul-Sharma-1234@users.noreply.github.com>
2022-05-09 17:56:41 -0700

    changes

--- web-frontend-service/src/pages/Login.jsx
+++ web-frontend-service/src/pages/Login.jsx
@@ -78,7 +78,6 @@ function Login() {
   return (
     <div className="app">
       <div className="login-form">
-        <div className="title">Sign In</div>
         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
       </div>
     </div>
